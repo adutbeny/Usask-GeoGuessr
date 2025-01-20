610f37f92575ff8e9e716648352ea9dfe3c9c7b0
Matt <matthewjberry03@gmail.com>
2025-01-20 17:25:32 -0600

    added subscriber interface and attached the view to it

--- src/main/java/org/example/cmpt370/View.java
+++ src/main/java/org/example/cmpt370/View.java
@@ -4,10 +4,16 @@ package org.example.cmpt370;
  * CMPT370
  */
 
-public class View {
+public class View implements Subscriber {
     /* Class that handles all display output
      * Needs to be updated by the Model each time
      * the user does something that requires the screen
      * to change (display new picture, map, etc)
      */
+
+
+    @Override
+    public void modelUpdated() {
+
+    }
 }
