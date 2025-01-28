fbed19d93c77055c33cef351b6682f5d733d6c15
Matt <matthewjberry03@gmail.com>
2025-01-28 13:56:17 -0600

    fixed stupid little import issue

--- src/main/java/org/example/cmpt370/Model.java
+++ src/main/java/org/example/cmpt370/Model.java
@@ -3,19 +3,18 @@ package org.example.cmpt370;
 /* Property of swagtown
  * CMPT370 */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /** Class that manages all the data
  * Controller calls Model methods to act on things like pictures
  * This is where we pass in the coordinates of mouse locations */
 public class Model {
 
-    private List<Subscriber> subscribers;
+    private ArrayList<Subscriber> subscribers;
 
     /** Constructor */
     public Model() {
-        this.subscribers = new ArrayList<>();
+        this.subscribers = new ArrayList<Subscriber>();
     }
 
     /** Add any displays to the list of objects updated on
