fc7d0137390d57f2f6ec3dd5fcf98dff867cdf40
Matt <matthewjberry03@gmail.com>
2025-02-11 13:54:52 -0600

    added view method to display new select difficulty window

--- src/main/java/org/example/cmpt370/View.java
+++ src/main/java/org/example/cmpt370/View.java
@@ -3,7 +3,8 @@ package org.example.cmpt370;
 /* Property of swagtown
  * CMPT370 */
 
-import javafx.geometry.Pos;
+import java.util.*;
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
@@ -23,12 +24,13 @@ public class View extends StackPane implements Subscriber {
 
     private Model model;
     private Canvas myCanvas;
+    private GraphicsContext gc;
 
     /** Constructor -
      * Runs all start up to create initial display when program starts */
     public View() {
         this.myCanvas = new Canvas(1200,800);
-        GraphicsContext gc = myCanvas.getGraphicsContext2D();
+        this.gc = myCanvas.getGraphicsContext2D();
 
         // background
         Image background = new Image("map2.jpeg");
@@ -63,6 +65,56 @@ public class View extends StackPane implements Subscriber {
         Button login = new Button("Log-In");
         login.setPrefWidth(200);
         Button createAcc = new Button("Create Account");
+        createAcc.setPrefWidth(200);
+
+        VBox buttonStack = new VBox(25, quickplay, login, createAcc);
+        // set below text
+        buttonStack.setTranslateX(300);
+        buttonStack.setTranslateY(350);
+
+        // add all to layout in order!!!
+        this.getChildren().addAll(bv, myCanvas, logo, buttonStack);
+    }
+
+    public void selectDifficultyWindow() {
+        // clear any objects in view
+        this.getChildren().setAll();
+        // reset canvas
+        gc.clearRect(0, 0, this.myCanvas.getWidth(), this.myCanvas.getHeight());
+
+        // background
+        Image background = new Image("map2.jpeg");
+        ImageView bv = new ImageView(background);
+        // Set the image to fill the window
+        bv.setFitWidth(1200);
+        bv.setFitHeight(800);
+        bv.setPreserveRatio(true);
+
+        // adds semi-transparent backing
+        gc.setFill(new Color(1, 1, 1, 0.5));
+        gc.fillRect(200, 0, 400, 800);
+
+        // logo
+        Image l = new Image("usaskcrest.png");
+        ImageView logo = new ImageView(l);
+        logo.setFitHeight(150);
+        logo.setFitWidth(150);
+        logo.setPreserveRatio(true);
+        logo.setTranslateX(-200);
+        logo.setTranslateY(-250);
+
+        // title
+        gc.setFill(Color.BLACK);
+        gc.setFont(new Font("Arial", 38));
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.fillText("Select Difficulty", 400, 250);
+
+        // setup buttons
+        Button quickplay = new Button("Novice Navigator");
+        quickplay.setPrefWidth(200);
+        Button login = new Button("Seasonal Searcher");
+        login.setPrefWidth(200);
+        Button createAcc = new Button("Expert Explorer");
         createAcc.setPrefWidth(200);
 
         VBox buttonStack = new VBox(25, quickplay, login, createAcc);
