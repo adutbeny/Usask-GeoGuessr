e065bc54b2e7e94fa21d9eaf9a8c9b0f34daf02e
adutb <adut.beny1@gmail.com>
2025-03-24 16:18:44 -0600

    Main menu showing again

--- src/main/java/org/example/cmpt370/View.java
+++ src/main/java/org/example/cmpt370/View.java
@@ -1268,7 +1268,7 @@ public class View extends StackPane implements Subscriber {
     /** Connect Model */
     public void setModel(Model m) {
         this.model = m;
-        createLeaderboardWindow();   // change this if you want to test a certain window
+        selectMainMenu();   // change this if you want to test a certain window
     }
 
     /**
