3f3e7b1004657960144ab80ad23879bfe654f4bb
jake <brw789@usask.ca>
2025-03-24 16:35:54 -0600

    WriteData() function for processing strings into json (writiing to firebase)

--- src/main/java/org/example/cmpt370/FirebaseHelper.java
+++ src/main/java/org/example/cmpt370/FirebaseHelper.java
@@ -11,4 +11,26 @@ public class FirebaseHelper {
     private static final String FIREBASE_DB_URL = "https://usask-geoguesser-default-rtdb.firebaseio.com/";
     private HttpClient client;
 
+    public FirebaseHelper() {
+        client = HttpClient.newHttpClient();
+    }
+
+    /** this is for writing data to the cloud server for multiplayer
+     * we format strings and then send it to the server to be kept as a json in a specified node
+     * takes two strings and returns nothing**/
+    public void writeData(String node, String jsonData) {
+        try {
+            String url = FIREBASE_DB_URL + "/" + node + ".json";
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .PUT(HttpRequest.BodyPublishers.ofString(jsonData))
+                    .header("Content-Type", "application/json")
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Write Response: " + response.body()); //for debugging
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
