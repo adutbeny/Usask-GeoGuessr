f0a4c017184ae2b31ec479413e01a8e619defbc8
jake <brw789@usask.ca>
2025-03-24 22:07:58 -0600

    added functions multiplayer queue, match creation, and recording guesses

--- src/main/java/org/example/cmpt370/Multiplayer.java
+++ src/main/java/org/example/cmpt370/Multiplayer.java
@@ -8,4 +8,22 @@ public class Multiplayer {
         this.playerUid = playerUid;
         fbHelper = new FirebaseHelper();
     }
+    /** joins the wating cue for multiplayer **/
+    public void joinWaitQueue() {
+        String waitingJson = "{\"timestamp\": " + System.currentTimeMillis() + "}";
+        fbHelper.writeData("waiting/" + playerUid, waitingJson);
+    }
+    /* creates a match in with all necessary nodes and writes to server */
+    public void createMatch(String opponentUid, String matchId) {
+        String matchJson = "{" + "\"players\": {" + "\"playerA\": \"" + playerUid + "\","
+                + "\"playerB\": \"" + opponentUid + "\"" + "}," + "\"guesses\": {}," + "\"chat\": {}" + "}";
+        fbHelper.writeData("matches/" + matchId, matchJson);
+    }
+
+    /** records the guess for a player and writes data to server */
+    public void recordGuess(String matchId, double lat, double lng) {
+        String guessJson = String.format("{ \"lat\": %f, \"lng\": %f }", lat, lng);
+        fbHelper.writeData("matches/" + matchId + "/guesses/" + playerUid, guessJson);
+    }
+
 }
