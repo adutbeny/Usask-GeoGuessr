09836a305cfa2deca39745d015b8ea88b013ccac
Matt <matthewjberry03@gmail.com>
2025-03-25 15:06:50 -0600

    changed chatwindow so that it launches its own window, changed some styling and added handling to the send button

--- src/main/java/org/example/cmpt370/ChatWindow.java
+++ src/main/java/org/example/cmpt370/ChatWindow.java
@@ -1,32 +1,37 @@
 package org.example.cmpt370;
 
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.*;
+import javafx.stage.Stage;
 
 /** Display class for the chat function
  * Needs to see model/server to get messages
  * Needs to be seen by the view to be displayed */
 public class ChatWindow extends StackPane {
-    // ChatWindow is a StackPane, holding a VBox with a scroll pane on top
-    // for messages and a textEntry on the bottom to send a message
-    private VBox messages;
-    private TextField entry;
-    private Button send;
-    // needs to see model and be seen by the view -> Setup
 
-    public ChatWindow() {
+    private VBox messages;          // stack of all messages
+    private TextField entry;        // entry field to type outgoing
+    private Button send;            // button to send
+    private Model model;        // takes model just to set send button handler
+
+    /** Constructs the ChatWindow, launching it as a separate window from View
+     * @param m take the model as an arg to set up handler */
+    public ChatWindow(Model m) {
         // the View will toggle this so we can hide the chat window during play
         this.setPrefSize(250, 300);
         this.setMinSize(250, 300);
         this.setMaxSize(250, 300);
-        this.setVisible(false);
+        this.model = m;
 
         this.messages = new VBox(10);
         this.messages.setPadding(new Insets(5));
@@ -36,13 +41,24 @@ public class ChatWindow extends StackPane {
         ScrollPane chat = new ScrollPane(this.messages);
         chat.setFitToWidth(true);
         chat.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        VBox.setVgrow(chat, Priority.ALWAYS);
 
         // message entry
         this.entry = new TextField();
         this.entry.setPrefWidth(180);
+        this.entry.setOnAction(_ -> this.send.fire());  // uses Enter key to trigger send
+
         this.send = new Button("Send");
-        this.send.setPrefSize(70, 50);
+        this.send.setPrefSize(70, 15);
         this.send.setStyle("-fx-font-size: 14px;");
+        this.send.setOnAction(_ -> {
+            if (!this.entry.getText().trim().isEmpty()) {
+                this.model.sendMessage(this.entry.getText());
+                this.entry.clear();
+                // set scroll to bottom
+                Platform.runLater(() -> chat.setVvalue(1.0));
+            }
+        });
         HBox entryField = new HBox(10, this.entry, this.send);
         entryField.setPadding(new Insets(5));
         entryField.setAlignment(Pos.CENTER_LEFT);
@@ -52,7 +68,15 @@ public class ChatWindow extends StackPane {
         container.setPadding(new Insets(5));
 
         this.getChildren().add(container);
-        this.show();
+
+        // Display in a separate window from View
+        Scene scene = new Scene(this, 250, 300);
+        scene.getStylesheets().addAll("buttonstyle.css");
+
+        Stage chatStage = new Stage();
+        chatStage.setScene(scene);
+        chatStage.setTitle("Chat");
+        chatStage.show();
     }
 
     /** Adds a message to itself for display
@@ -62,7 +86,6 @@ public class ChatWindow extends StackPane {
 
         Text text = new Text(msg);
         text.setFont(Font.font("Segoe UI This", FontWeight.NORMAL, 14));
-        //text.setFill(Color.WHITE);
 
         // Wrap text in a TextFlow for formatting
         TextFlow textFlow = new TextFlow(text);
