c3340f11868cf2e74561bd869c6c363d51a20057
Matt <matthewjberry03@gmail.com>
2025-03-25 15:07:05 -0600

    changed multiplayer button handling to new method

--- src/main/java/org/example/cmpt370/Controller.java
+++ src/main/java/org/example/cmpt370/Controller.java
@@ -124,7 +124,7 @@ public class Controller {
             view.submit.setVisible(true);
         });
         view.multiplayer.setOnAction(event ->{
-            this.model.showMatchmakingWindow();
+            this.model.initiateMultiplayer();
         });
         view.addPin.setOnAction(event -> {
             this.model.pin();
