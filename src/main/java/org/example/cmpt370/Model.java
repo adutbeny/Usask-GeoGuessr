c869b8ed0a4cc60a6e5c5c2c4b09d12ec80e6633
jake <brw789@usask.ca>
2025-03-28 12:45:36 -0600

    changed control flow of matchmaking

--- src/main/java/org/example/cmpt370/Model.java
+++ src/main/java/org/example/cmpt370/Model.java
@@ -841,8 +841,6 @@ public class Model {
             if (matchId != null) {
                 setCurrentMatchId(matchId);
                 System.out.println("match id is: " + matchId);
-                this.showGameplayWindow();
-                notifySubscribers(); // update view
                 scheduler.shutdown(); // stop polling
                 return;
             }
@@ -852,13 +850,13 @@ public class Model {
             if (matchId != null) {
                 setCurrentMatchId(matchId);
                 System.out.println("match found for opponent: " + matchId);
-                this.showGameplayWindow();
-                notifySubscribers(); // update view
                 scheduler.shutdown(); // stop polling
             } else {
                 System.out.println("still waiting for a match...");
             }
         }, 0, 3, TimeUnit.SECONDS); // this is the polling time we can change it
+        this.showGameplayWindow();
+        notifySubscribers(); // update view
     }
 
     /**  multiplayer version of calculate score will need to adjust a lot **/
